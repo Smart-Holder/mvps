2b4792267b02137d51eed2f65151ef9d95a1e9c5
MagnumGOYB <sosbs@outlook.com>
2023-07-03 17:27:16 +0800

    feat: update

--- src/asset.entity.ts
+++ src/asset.entity.ts
@@ -7,6 +7,21 @@ export const AssetTypeEnum: Record<string, number> = {
   [ErcType.ERC_1155]: 2
 }
 
+export function formatHex(hex_str: string | number | bigint, btyes = 32) {
+  let s = ''
+  if (typeof hex_str == 'string') {
+    s = BigInt(hex_str).toString(16)
+  } else {
+    s = hex_str.toString(16)
+  }
+  const len = btyes * 2 - s.length
+  if (len > 0) {
+    return '0x' + Array.from({ length: len + 1 }).join('0') + s
+  } else {
+    return '0x' + s
+  }
+}
+
 export class AssetEntity {
   token: string
   tokenId: string
@@ -27,7 +42,7 @@ export class AssetEntity {
 
   constructor(source: EvmAsset) {
     this.token = source.contract_address
-    this.tokenId = source.token_id
+    this.tokenId = formatHex(source.token_id)
     this.count = source.ownsTotal
     this.uri = source.token_uri
     this.name = source.name || source.contract_name
