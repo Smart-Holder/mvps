8f3a8f4512b03f15ddbcc5d19a23c0b8529c548a
MagnumGOYB <sosbs@outlook.com>
2023-07-10 17:49:34 +0800

    feat: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,31 @@
-export default {
-  externals: []
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('webpack')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  externals: [],
+  plugins: [
+    new webpack.IgnorePlugin({
+      checkResource(resource) {
+        const lazyImports = [
+          '@nestjs/microservices',
+          '@nestjs/microservices/microservices-module',
+          '@nestjs/websockets',
+          '@nestjs/websockets/socket-module'
+        ]
+        if (!lazyImports.includes(resource)) {
+          return false
+        }
+        try {
+          require.resolve(resource, {
+            paths: [process.cwd()]
+          })
+        } catch (err) {
+          return true
+        }
+        return false
+      }
+    }),
+    new ForkTsCheckerWebpackPlugin()
+  ]
 }
