097172520fc7294311e89ad9caddbc1ff7b3c6d0
MagnumGOYB <sosbs@outlook.com>
2023-11-10 17:31:44 +0800

    feat: fix image uri

--- src/asset.entity.ts
+++ src/asset.entity.ts
@@ -24,6 +24,10 @@ export class AssetEntity {
   metadataJson: Record<string, any>
 
   constructor(source: EvmAsset) {
+    let metadataJson = {} as { image?: string }
+    try {
+      metadataJson = JSON.parse(source.metadata_json)
+    } catch (error) {}
     this.token = source.contract_address
     this.chain = source.chain
     this.tokenId = formatHex(source.token_id)
@@ -33,15 +37,55 @@ export class AssetEntity {
     this.name = source.name || source.contract_name
     this.type = AssetTypeEnum[source.erc_type]
     this.author = source.minter
-    this.media = source.nftscan_uri || source.content_uri || ''
+    let mediaUri = ''
+    if (
+      source.nftscan_uri &&
+      source.nftscan_uri.startsWith('http') &&
+      !source.nftscan_uri.startsWith('ipfs')
+    ) {
+      mediaUri = source.nftscan_uri
+    } else if (
+      source.content_uri &&
+      source.content_uri.startsWith('http') &&
+      !source.content_uri.startsWith('ipfs')
+    ) {
+      mediaUri = source.content_uri
+    }
+    if (!mediaUri.startsWith('http') && !mediaUri.startsWith('/') && mediaUri) {
+      mediaUri = `https://ipfs.io/ipfs/${mediaUri}`
+    }
+    this.media = mediaUri || source.content_uri || ''
     this.mediaOrigin = source.content_uri || ''
-    this.image = source.nftscan_uri || source.image_uri || ''
+    let imgUri = ''
+    if (
+      source.nftscan_uri &&
+      source.nftscan_uri.startsWith('http') &&
+      !source.nftscan_uri.startsWith('ipfs')
+    ) {
+      imgUri = source.image_uri
+    } else if (
+      source.image_uri &&
+      source.image_uri.startsWith('http') &&
+      !source.image_uri.startsWith('ipfs')
+    ) {
+      imgUri = source.image_uri
+    } else if (
+      metadataJson.image &&
+      metadataJson.image.startsWith('http') &&
+      !metadataJson.image.startsWith('ipfs')
+    ) {
+      imgUri = metadataJson.image
+    }
+    if (!imgUri.startsWith('http') && !imgUri.startsWith('/') && imgUri) {
+      imgUri = `https://ipfs.io/ipfs/${imgUri}`
+    }
+    this.image = imgUri || source.image_uri || ''
     this.imageOrigin = source.image_uri || ''
     this.thumbnail = source.small_nftscan_uri || ''
     this.externalLink = source.external_link || ''
     this.symbol = source.symbol
     this.info = source.description
-    this.metadataJson = JSON.parse(source.metadata_json)
+    this.metadataJson = metadataJson
     this.owner = source.owner
     this.ownerBase = source.ownerBase
   }
